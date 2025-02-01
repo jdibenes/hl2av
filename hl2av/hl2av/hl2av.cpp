98a4a4a6f0f31b67c28d00664d15d514ddfe541c
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-02-01 14:12:17 -0500

    plugin update

--- hl2av/hl2av/hl2av.cpp
+++ hl2av/hl2av/hl2av.cpp
@@ -11,13 +11,13 @@
 //-----------------------------------------------------------------------------
 
 HL2AV_EXPORT
-void hl2av_core_startup()
+void core_startup()
 {
     Core_Startup();
 }
 
 HL2AV_EXPORT
-void hl2av_core_shutdown()
+void core_shutdown()
 {
     Core_Shutdown();
 }
@@ -27,31 +27,37 @@ void hl2av_core_shutdown()
 //-----------------------------------------------------------------------------
 
 HL2AV_EXPORT
-void* hl2av_sample_create(void*& base, uint32_t size)
+void* sample_create(void*& base, uint32_t size)
 {
     return Sample_Create(base, size);
 }
 
 HL2AV_EXPORT
-void hl2av_sample_setinfo(void* self, int64_t time, int64_t duration)
+void sample_setinfo(void* self, int64_t time, int64_t duration)
 {
     Sample_SetInfo(self, time, duration);
 }
 
 HL2AV_EXPORT
-void hl2av_sample_getbase(void* self, void*& base, uint32_t& size)
+void sample_getbase(void* self, void*& base, uint32_t& size)
 {
     Sample_GetBase(self, base, size);
 }
 
 HL2AV_EXPORT
-void hl2av_sample_getinfo(void* self, int64_t& time, int64_t& duration, uint32_t& cleanpoint)
+void sample_getinfo(void* self, int64_t& time, int64_t& duration, uint32_t& cleanpoint)
 {
     Sample_GetInfo(self, time, duration, cleanpoint);
 }
 
 HL2AV_EXPORT
-void hl2av_sample_release(void* self)
+void sample_getresolution(void* self, uint32_t& width, uint32_t& stride, uint32_t& height)
+{
+    Sample_GetResolution(self, width, stride, height);
+}
+
+HL2AV_EXPORT
+void sample_release(void* self)
 {
     Sample_Release(self);
 }
@@ -61,37 +67,37 @@ void hl2av_sample_release(void* self)
 //-----------------------------------------------------------------------------
 
 HL2AV_EXPORT
-void* hl2av_encoder_createforvideo(uint16_t width, uint16_t stride, uint16_t height, uint8_t framerate, uint8_t divisor, uint8_t subtype, uint8_t profile, int8_t level, uint32_t bitrate)
+void* encoder_createforvideo(uint16_t width, uint16_t stride, uint16_t height, uint8_t framerate, uint8_t divisor, uint8_t subtype, uint8_t profile, int8_t level, uint32_t bitrate)
 {
     return Encoder_CreateForVideo(width, stride, height, framerate, divisor, (VideoSubtype)subtype, (H26xProfile)profile, level, bitrate);
 }
 
 HL2AV_EXPORT
-void* hl2av_encoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t subtype, uint8_t profile, uint8_t level)
+void* encoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t subtype, uint8_t profile, uint8_t level)
 {
     return Encoder_CreateForAudio(channels, samplerate, (AudioSubtype)subtype, (AACProfile)profile, (AACLevel)level);
 }
 
 HL2AV_EXPORT
-void hl2av_encoder_push(void* self, void* sample)
+void encoder_push(void* self, void* sample)
 {
     Encoder_Push(self, sample);
 }
 
 HL2AV_EXPORT
-uint8_t hl2av_encoder_peek(void* self)
+uint8_t encoder_peek(void* self)
 {
     return Encoder_Peek(self);
 }
 
 HL2AV_EXPORT
-void* hl2av_encoder_pull(void* self)
+void* encoder_pull(void* self)
 {
     return Encoder_Pull(self);
 }
 
 HL2AV_EXPORT
-void hl2av_encoder_destroy(void* self)
+void encoder_destroy(void* self)
 {
     Encoder_Destroy(self);
 }
@@ -101,37 +107,37 @@ void hl2av_encoder_destroy(void* self)
 //-----------------------------------------------------------------------------
 
 HL2AV_EXPORT
-void* hl2av_decoder_createforvideo(uint8_t profile, uint8_t subtype)
+void* decoder_createforvideo(uint8_t profile, uint8_t subtype)
 {
     return Decoder_CreateForVideo((H26xProfile)profile, (VideoSubtype)subtype);
 }
 
 HL2AV_EXPORT
-void* hl2av_decoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t profile, uint8_t subtype)
+void* decoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t profile, uint8_t subtype)
 {
     return Decoder_CreateForAudio(channels, samplerate, (AACProfile)profile, (AudioSubtype)subtype);
 }
 
 HL2AV_EXPORT
-void hl2av_decoder_push(void* self, void* sample)
+void decoder_push(void* self, void* sample)
 {
     Decoder_Push(self, sample);
 }
 
 HL2AV_EXPORT
-bool hl2av_decoder_peek(void* self)
+uint8_t decoder_peek(void* self)
 {
     return Decoder_Peek(self);
 }
 
 HL2AV_EXPORT
-void* hl2av_decoder_pull(void* self)
+void* decoder_pull(void* self)
 {
     return Decoder_Pull(self);
 }
 
 HL2AV_EXPORT
-void hl2av_decoder_destroy(void* self)
+void decoder_destroy(void* self)
 {
     Decoder_Destroy(self);
 }
--- hl2av/hl2av/hl2av.h
+++ hl2av/hl2av/hl2av.h
@@ -10,70 +10,73 @@
 //-----------------------------------------------------------------------------
 
 HL2AV_IMPORT
-void hl2av_core_startup();
+void core_startup();
 
 HL2AV_IMPORT
-void hl2av_core_shutdown();
+void core_shutdown();
 
 //-----------------------------------------------------------------------------
 // Sample
 //-----------------------------------------------------------------------------
 
 HL2AV_IMPORT
-void* hl2av_sample_create(void*& base, uint32_t size);
+void* sample_create(void*& base, uint32_t size);
 
 HL2AV_IMPORT
-void hl2av_sample_setinfo(void* self, int64_t time, int64_t duration);
+void sample_setinfo(void* self, int64_t time, int64_t duration);
 
 HL2AV_IMPORT
-void hl2av_sample_getbase(void* self, void*& base, uint32_t& size);
+void sample_getbase(void* self, void*& base, uint32_t& size);
 
 HL2AV_IMPORT
-void hl2av_sample_getinfo(void* self, int64_t& time, int64_t& duration, uint32_t& cleanpoint);
+void sample_getinfo(void* self, int64_t& time, int64_t& duration, uint32_t& cleanpoint);
 
 HL2AV_IMPORT
-void hl2av_sample_release(void* self);
+void sample_getresolution(void* self, uint32_t& width, uint32_t& stride, uint32_t& height);
+
+HL2AV_IMPORT
+void sample_release(void* self);
 
 //-----------------------------------------------------------------------------
 // Encoder
 //-----------------------------------------------------------------------------
 
 HL2AV_IMPORT
-void* hl2av_encoder_createforvideo(uint16_t width, uint16_t stride, uint16_t height, uint8_t framerate, uint8_t divisor, uint8_t subtype, uint8_t profile, int8_t level, uint32_t bitrate);
+void* encoder_createforvideo(uint16_t width, uint16_t stride, uint16_t height, uint8_t framerate, uint8_t divisor, uint8_t subtype, uint8_t profile, int8_t level, uint32_t bitrate);
 
 HL2AV_IMPORT
-void* hl2av_encoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t subtype, uint8_t profile, uint8_t level);
+void* encoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t subtype, uint8_t profile, uint8_t level);
 
 HL2AV_IMPORT
-void hl2av_encoder_push(void* self, void* sample);
+void encoder_push(void* self, void* sample);
 
 HL2AV_IMPORT
-uint8_t hl2av_encoder_peek(void* self);
+uint8_t encoder_peek(void* self);
 
 HL2AV_IMPORT
-void* hl2av_encoder_pull(void* self);
+void* encoder_pull(void* self);
 
 HL2AV_IMPORT
-void hl2av_encoder_destroy(void* self);
+void encoder_destroy(void* self);
 
 //-----------------------------------------------------------------------------
 // Decoder
 //-----------------------------------------------------------------------------
 
 HL2AV_IMPORT
-void* hl2av_decoder_createforvideo(uint8_t profile, uint8_t subtype);
+void* decoder_createforvideo(uint8_t profile, uint8_t subtype);
 
 HL2AV_IMPORT
-void* hl2av_decoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t profile, uint8_t subtype);
+void* decoder_createforaudio(uint8_t channels, uint16_t samplerate, uint8_t profile, uint8_t subtype);
 
 HL2AV_IMPORT
-void hl2av_decoder_push(void* self, void* sample);
+void decoder_push(void* self, void* sample);
 
 HL2AV_IMPORT
-bool hl2av_decoder_peek(void* self);
+uint8_t decoder_peek(void* self);
 
 HL2AV_IMPORT
-void* hl2av_decoder_pull(void* self);
+void* decoder_pull(void* self);
 
 HL2AV_IMPORT
-void hl2av_decoder_destroy(void* self);
+void decoder_destroy(void* self);
