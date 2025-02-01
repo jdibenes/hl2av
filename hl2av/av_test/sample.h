264a39dbd9e812ead7c879d4220fff7854ad9c46
jdibenes <58836755+jdibenes@users.noreply.github.com>
2025-02-01 13:58:32 -0500

    Update sample.h

--- hl2av/av_test/sample.h
+++ hl2av/av_test/sample.h
@@ -7,4 +7,5 @@ void* Sample_Create(void*& base, uint32_t size);
 void Sample_SetInfo(void* self, int64_t time, int64_t duration);
 void Sample_GetBase(void* self, void*& base, uint32_t& size);
 void Sample_GetInfo(void* self, int64_t& time, int64_t& duration, uint32_t& cleanpoint);
+void Sample_GetResolution(void* self, uint32_t& width, uint32_t& stride, uint32_t& height);
 void Sample_Release(void* self);
